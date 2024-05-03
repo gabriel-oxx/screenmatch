21e47ec84cb38eb62850e1474a030f234bf4576d
Gabriel <gabrielcsilvasg@gmail.com>
2024-05-03 19:15:24 -0300

    Adicionar registro pra tratar dados da série

--- src/main/java/br/com/media/screenmatch/models/SerieData.java
+++ src/main/java/br/com/media/screenmatch/models/SerieData.java
@@ -3,5 +3,5 @@ package br.com.media.screenmatch.models;
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 
 @JsonIgnoreProperties(ignoreUnknown = true)
-public record DataSerie(String Title, String Year, Integer totalSeasons, String imdbRating) {
+public record SerieData(String Title, String Year, Integer totalSeasons, String imdbRating) {
 }
