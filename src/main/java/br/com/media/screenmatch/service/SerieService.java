1b967bb96e03c427975b85fe1d4cadc6da7adc8a
Gabriel <gabrielcsilvasg@gmail.com>
2024-05-11 00:39:20 -0300

    Adicionar classe RatingService

---
+++ src/main/java/br/com/media/screenmatch/service/SerieService.java
@@ -0,0 +1,33 @@
+ package br.com.media.screenmatch.service;
+
+import br.com.media.screenmatch.config.DataConfig;
+import br.com.media.screenmatch.models.SeasonData;
+import br.com.media.screenmatch.models.SerieData;
+
+import java.util.List;
+import java.util.ArrayList;
+
+public class SerieService {
+	private ApiConsumption consumption = new ApiConsumption();
+	private DataConverter converter = new DataConverter();
+	private DataConfig dataConfig = new DataConfig();
+private final String ADDRESS = "https://www.omdbapi.com/?t=";
+	private final String API_KEY = dataConfig.getConfig();
+
+	public List<SeasonData> getSeasons(String serieName) {
+		String url = ADDRESS + serieName.replace(" ", "+") + "&apikey=" + API_KEY;
+		String json = consumption.getData(url);
+		SerieData serie = converter.getData(json, SerieData.class);
+		List<SeasonData> seasons = new ArrayList<>();
+
+		for (int i = 1; i <= serie.totalSeasons(); i++) {
+			json = consumption.getData(ADDRESS + serieName.replace(" ", "+") + "&season=" + i + "&apikey=" + API_KEY);
+			SeasonData seasonData = converter.getData(json, SeasonData.class);
+			seasons.add(seasonData);
+		}
+
+		return seasons;
+	}
+
+
+}
