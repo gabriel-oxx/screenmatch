d736b76cafb53f60b59758d197703b2f37debbb3
Gabriel <gabrielcsilvasg@gmail.com>
2024-05-12 06:50:25 -0300

    Adicionar getters e setters e sobre carregar o Adicionar impressão a partir da lista de séries

--- src/main/java/br/com/media/screenmatch/service/SerieService.java
+++ src/main/java/br/com/media/screenmatch/service/SerieService.java
@@ -1,11 +1,13 @@
 package br.com.media.screenmatch.service;
 
 import br.com.media.screenmatch.config.DataConfig;
-import br.com.media.screenmatch.models.SeasonData;
+import br.com.media.screenmatch.models.Serie;
 import br.com.media.screenmatch.models.SerieData;
 
+import java.util.Comparator;
 import java.util.List;
 import java.util.ArrayList;
+import java.util.stream.Collectors;
 
 public class SerieService {
 	private ApiConsumption consumption = new ApiConsumption();
@@ -13,19 +15,26 @@ public class SerieService {
 	private DataConfig dataConfig = new DataConfig();
 	private final String ADDRESS = "https://www.omdbapi.com/?t=";
 	private final String API_KEY = dataConfig.getConfig();
-	private List<SerieData> series = new ArrayList<>();
+	private List<SerieData> seriesData = new ArrayList<>();
 	SerieData data;
+
 
 	public void searchWebSerie(String serieName) {
 		String url = ADDRESS + serieName.replace(" ", "+") + "&apikey=" + API_KEY;
 		String json = consumption.getData(url);
 		data = converter.getData(json, SerieData.class);
 		System.out.println(data.title() + " - " + data.year() + ", avaliação média no IMDB: " + data.imdbRating() + ", contém " + data.totalSeasons() + " temporadas.");
-		series.add(data);
+		seriesData.add(data);
 	}
 
 	public void listSeries() {
-		series.forEach(System.out::println);
+		List<Serie> series = new ArrayList<>();
+series = seriesData.stream()
+		.map(d -> new Serie(d))
+		.collect(Collectors.toList());
+series.stream()
+		.sorted(Comparator.comparing(Serie::getGenre))
+		.forEach(System.out::println);
 	}
 
 
