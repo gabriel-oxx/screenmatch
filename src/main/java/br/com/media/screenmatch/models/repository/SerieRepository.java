c7f427b0f6a3d57432096e135a3a407bcdab4854
Gabriel <gabrielcsilvasg@gmail.com>
2024-06-08 21:45:31 -0300

    Adicionar consultas derivadas para pesquisar série por título e por ano

--- src/main/java/br/com/media/screenmatch/models/repository/SerieRepository.java
+++ src/main/java/br/com/media/screenmatch/models/repository/SerieRepository.java
@@ -3,6 +3,14 @@ package br.com.media.screenmatch.models.repository;
 import br.com.media.screenmatch.models.Serie;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+import java.util.Optional;
+
 
 public interface SerieRepository extends JpaRepository<Serie, Long> {
+	Optional<Serie> findByTitleContainingIgnoreCase(String title);
+
+	List<Serie> findByYearContaining(String year);
+
+
 }
