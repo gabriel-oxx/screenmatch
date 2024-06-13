938f9cf1c828541571242d705c7bb5be55c1f56a
Gabriel <gabrielcsilvasg@gmail.com>
2024-06-13 03:09:56 -0300

    Reestruturar classes, criando classe SerieService para lidar com o banco de dados e criação das rotas, deixando com a classe SerieController o dever de executar as rotas

--- src/main/java/br/com/media/screenmatch/controller/SerieController.java
+++ src/main/java/br/com/media/screenmatch/controller/SerieController.java
@@ -1,36 +1,21 @@
 package br.com.media.screenmatch.controller;
 
 import br.com.media.screenmatch.dto.SerieDto;
-import br.com.media.screenmatch.models.Serie;
-import br.com.media.screenmatch.models.repository.SerieRepository;
+import br.com.media.screenmatch.service.SerieService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
-import java.util.stream.Collectors;
 
 @RestController
 public class SerieController {
 	@Autowired
-	private SerieRepository repository;
+	private SerieService service;
 
 	@GetMapping("/series")
 	public List<SerieDto> getSeries() {
-		return repository.findAll()
-				.stream()
-				.map(s -> new SerieDto(
-						s.getId(),
-						s.getTitle(),
-						s.getYear(),
-						s.getGenre(),
-						s.getTotalSeasons(),
-						s.getPlot(),
-						s.getActores(),
-						s.getImdbRating(),
-						s.getPost()
-				))
-				.collect(Collectors.toList());
+		return service.getAllSeries();
 	}
 
 
--- src/main/java/br/com/media/screenmatch/service/SerieService.java
+++ src/main/java/br/com/media/screenmatch/service/SerieService.java
@@ -1,51 +1,32 @@
-/*
 package br.com.media.screenmatch.service;
 
-import br.com.media.screenmatch.config.DataConfig;
-import br.com.media.screenmatch.models.Serie;
-import br.com.media.screenmatch.models.SerieData;
+import br.com.media.screenmatch.dto.SerieDto;
+import br.com.media.screenmatch.models.repository.SerieRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
-import java.net.URISyntaxException;
-import java.util.Comparator;
 import java.util.List;
-import java.util.ArrayList;
 import java.util.stream.Collectors;
 
+@Service
 public class SerieService {
-	private ApiConsumption consumption = new ApiConsumption();
-	private DataConverter converter = new DataConverter();
-	private DataConfig dataConfig = new DataConfig();
-	private final String ADDRESS = "https://www.omdbapi.com/?t=";
-	private final String API_KEY = "c981cb6e";
-	private List<SerieData> seriesData = new ArrayList<>();
-	SerieData data;
+	@Autowired
+	private SerieRepository repository;
 
-
-	public void searchWebSerie(String serieName) {
-		String url = ADDRESS + serieName.replace(" ", "+") + "&apikey=" + API_KEY;
-		String json = consumption.getData(url);
-		data = converter.getData(json, SerieData.class);
-		System.out.println(data.title() + " - " + data.year() + ", avaliação média no IMDB: " + data.imdbRating() + ", contém " + data.totalSeasons() + " temporadas.");
-		seriesData.add(data);
+	public List<SerieDto> getAllSeries(){
+		return repository.findAll()
+				.stream()
+				.map(s -> new SerieDto(
+						s.getId(),
+						s.getTitle(),
+						s.getYear(),
+						s.getGenre(),
+						s.getTotalSeasons(),
+						s.getPlot(),
+						s.getActores(),
+						s.getImdbRating(),
+						s.getPost()
+				))
+				.collect(Collectors.toList());
 	}
-
-	public void listSeries() {
-		List<Serie> series = new ArrayList<>();
-series = seriesData.stream()
-		.map(d -> {
-			try {
-				return new Serie(d);
-			} catch (URISyntaxException e) {
-				throw new RuntimeException(e);
-			}
-		})
-		.collect(Collectors.toList());
-series.stream()
-		.sorted(Comparator.comparing(Serie::getGenre))
-		.forEach(System.out::println);
-	}
-
-
-
 }
-*/
